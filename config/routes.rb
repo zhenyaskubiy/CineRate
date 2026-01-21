67ac97391edbd52a896b1b848f6997bd1137691a
Yevheniia Skubii <yevgeniyaskubiy@gmail.com>
2026-01-21 16:11:18 +0200

    Add simple header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "pages/home"
   get "up" => "rails/health#show", as: :rails_health_check
+  root "pages#home"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # get "top_10", to: "pages#top_10"
+  # get "trending", to: "pages#trending"
+  # get "what_to_watch", to: "pages#what_to_watch"
 end
